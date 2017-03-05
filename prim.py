10ae44c2ec3c08fdbf0764ac0d2ab34453f3bd84
rcreagh <rcreagh@rcreagh-macbookair.roam.corp.google.com>
2017-03-05 15:07:47 +0000

    add prims algo

---
+++ prim.py
@@ -0,0 +1,16 @@
+#! usr/bin/python
+"""This script is an implementation of Prim's greedy algorithm for learning
+purposes. Prim's algorithm is a greedy approach to finding a (there may be more
+than one) minimal spanning tree of a graph, using Python's networkx library."""
+
+import networkx
+
+def prim(graph):
+  """ Implementation of Prim's Greedy Algorithm.
+
+  Args:
+    graph: Graph which we are trying to find a minimal spanning tree for.
+  """
+
+  # Initialize count of the number of nodes in the tree.
+  current_tree_size = 0
